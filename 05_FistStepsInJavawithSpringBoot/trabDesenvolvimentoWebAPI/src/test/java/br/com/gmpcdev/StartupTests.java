6427336b5f7f15870e16a5f13302411e29f94035
GuilhermeMPCunha <guilhermecunha10@hotmail.com>
2024-05-04 11:45:32 -0300

    Fist Steps in Java with Spring Boot 3

---
+++ 05_FistStepsInJavawithSpringBoot/trabDesenvolvimentoWebAPI/src/test/java/br/com/gmpcdev/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.gmpcdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
